3cc5999423f456bb71d3f67757bfb10d079d91c2
utku3310 <111754510+utku3310@users.noreply.github.com>
2024-06-26 12:00:51 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
 
     token: "MTA2NTMwMDEwMTY0NTQ4NDE4NA.GiHTfV.2X7ILhsyKWTlxBTJZHRV4ywbvuDe7484ZXxqFs", //bot tokeniniz
-    clientId: "1065300101645484184", //Bot idsi
+    clientId: "1198004330830254120", //Bot idsi
     key: "K84530988688957", //ocr.space key link : https://ocr.space
 
     //Alttakileri elleme !
